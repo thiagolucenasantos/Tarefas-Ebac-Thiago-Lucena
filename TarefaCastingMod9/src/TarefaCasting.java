7dc02ce206db5fee44e5a1a4a15c57bf3e960aa4
Thiago <tlucenalive@gmail.com>
2023-10-12 11:39:41 -0300

    Exercicio de casting modulo 9 correção

--- TarefaCastingMod9/src/TarefaCasting.java
+++ TarefaCastingMod9/src/TarefaCasting.java
@@ -1,10 +1,10 @@
 public class TarefaCasting {
     public static void main(String[] args) {
-        
+
         int numero = 10;
         System.out.println(numero);
 
-        long numeroMaior =  numero;
+        Integer numeroMaior =  numero;
         System.out.println(numeroMaior);
 
     }
