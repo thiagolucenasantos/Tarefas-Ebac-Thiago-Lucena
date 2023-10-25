1d3bd2ccc7882daeee27dd2ac8d6559240323a6b
Thiago <tlucenalive@gmail.com>
2023-10-25 11:54:54 -0300

    Exercicio Modulo 14 Crud

--- ExemploMod15/src/br/com/thiago/abstractFactory/FuscaCar.java
+++
@@ -1,7 +0,0 @@
-package br.com.thiago.abstractFactory;
-
-public class FuscaCar extends Car{
-    public FuscaCar(int horsePower, String fuelSource, String color) {
-        super(horsePower, fuelSource, color);
-    }
-}
